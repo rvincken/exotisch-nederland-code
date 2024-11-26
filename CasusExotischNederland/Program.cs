9fe06e9d9e9086f3ed94062e2ff5e7336b91c54a
rckrlz <curiousgeorge24@protonmail.com>
2024-11-26 14:59:29 +0100

    Init

---
+++ CasusExotischNederland/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CasusExotischNederland
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
