e56a1266ea5c0e96711c001ef97097c7e3f9531d
rvincken <2403211vincken@zuyd.nl>
2024-11-26 15:32:42 +0100

    test 6

--- CasusExotischNederland/src/Program.cs
+++ CasusExotischNederland/src/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("TEMPORARY MESSAGE 2");
+            Console.WriteLine("TEMPORARY MESSAGE");
         }
     }
 }
