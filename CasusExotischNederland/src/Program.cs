2d497009f77af5f5817bd97fb5fe60a125c8efed
rvincken <2403211vincken@zuyd.nl>
2024-11-28 11:41:16 +0100

    removed unnecessary 'public' infront of main function

--- CasusExotischNederland/src/Program.cs
+++ CasusExotischNederland/src/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
         }
